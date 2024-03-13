aae4fb79f585c2a580b85e1341ce6d0061552104
Tim Ward <wardtd@gmail.com>
2024-03-13 16:11:46 -0400

    Simplified blocking function

--- src/block_nodes.py
+++ src/block_nodes.py
@@ -3,19 +3,10 @@ from textnode import TextNode
 
 def markdown_to_blocks(markdown):
     blocks = []
-    strings = markdown.split("\n")
-    offset = 0
-    current = ""
-    for i in range(0, len(strings)):
-        strings[i+ offset] = strings[i + offset].strip()
-        if strings[i + offset] == "":
-            strings.remove(strings[i + offset])
-            offset -= 1
-            if current != "":
-                blocks.append(current)
-                current = ""
-        else:
-            if current != "":
-                current += "\n"
-            current += strings[i + offset]
+    strings = markdown.split("\n\n")
+    for string in strings:
+        if string == "":
+            continue
+        string = string.strip()
+        blocks.append(string)
     return blocks
--- src/test_block_nodes.py
+++ src/test_block_nodes.py
@@ -13,15 +13,32 @@ text_type_image = "image"
 class TestBlockNodes(unittest.TestCase):
 
     def test_markdown_to_blocks(self):
-        markdown = '''# This is a heading
+        markdown = '''
+        # This is a heading
 
-        This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
-        * This is a list item
-        * This is another list item
+* This is a list item
+* This is another list item
         '''
-        # print(markdown.split("\n"))
-        # print(f"Blocks: {markdown_to_blocks(markdown)}")
         self.assertEqual(markdown_to_blocks(markdown), ["# This is a heading",
                                                          "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
                                                          "* This is a list item\n* This is another list item"])
+    
+
+    def test_markdown_to_blocks_longer(self):
+        markdown = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items"""
+        self.assertEqual(markdown_to_blocks(markdown), ["This is **bolded** paragraph",
+                                                         "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                                                         "* This is a list\n* with items"])
+    
