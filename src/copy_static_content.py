b01adff1035449451eb459f04e824495d4c32dc6
Tim Ward <wardtd@gmail.com>
2024-03-14 14:51:50 -0400

    Moved the copy_directory_contents function into its own module to clean up main

---
+++ src/copy_static_content.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+
+
+def copy_directory_contents(source, target):
+    if not os.path.exists(target):
+        os.mkdir(target)
+    if not os.path.exists(source):
+        raise Exception("Unable to access source directory")
+    source_items = os.listdir(source)
+    for item in source_items:
+        print(f"Current item: {item}")
+        if os.path.isfile(os.path.join(source, item)):
+            print(f"{item} is a file. Copying to {target}")
+            shutil.copy(os.path.join(source, item), os.path.join(target, item))
+        else:
+            print(f"{item} is a directory. Creating {os.path.join(target, item)}")
+            os.mkdir(os.path.join(target, item))
+            print(f"Copying the contents of {os.path.join(source, item)} to {os.path.join(target, item)}")
+            copy_directory_contents(os.path.join(source, item), os.path.join(target, item))
--- src/main.py
+++ src/main.py
@@ -1,28 +1,11 @@
-import os
 import shutil
-from inline_nodes import text_node_to_html_node
-from textnode import TextNode
+from copy_static_content import copy_directory_contents
 
 
 
-def copy_directory_contents(source, target):
-    print(f"Clearing the {target} directory")
-    shutil.rmtree(target)
-    os.mkdir(target)
-    if not os.path.exists(source):
-        raise Exception("Unable to access source directory")
-    source_items = os.listdir(source)
-    for item in source_items:
-        print(f"Current item: {item}")
-        if os.path.isfile(os.path.join(source, item)):
-            print(f"{item} is a file. Copying to {target}")
-            shutil.copy(os.path.join(source, item), os.path.join(target, item))
-        else:
-            print(f"{item} is a directory. Creating {os.path.join(target, item)}")
-            os.mkdir(os.path.join(target, item))
-            print(f"Copying the contents of {os.path.join(source, item)} to {os.path.join(target, item)}")
-            copy_directory_contents(os.path.join(source, item), os.path.join(target, item))
 def main():
+    print(f"Clearing the public directory")
+    shutil.rmtree("public")
     copy_directory_contents("static", "public")
 
 
